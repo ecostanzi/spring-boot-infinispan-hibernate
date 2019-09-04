695760d66e36150bf6801f6b196f2195b11b097f
Enrico Costanzi <enrico.costanzi@gmail.com>
2019-09-04 09:44:00 +0200

    first commit

---
+++ src/main/java/com/example/demoinfinispan/DemoinfinispanApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoinfinispan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoinfinispanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoinfinispanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoinfinispan/DemoinfinispanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demoinfinispan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoinfinispanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
